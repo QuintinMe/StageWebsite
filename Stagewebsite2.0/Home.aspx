5fa9c8f56c45fefba0e4d9979f5a9ffb8258ec23
Joy <joyhaha@hotmail.nl>
2017-04-05 13:37:45 +0200

    Aangepaste masterpage

--- Stagewebsite2.0/Home.aspx
+++ Stagewebsite2.0/Home.aspx
@@ -1,6 +1,7 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Stage.Master" AutoEventWireup="true" CodeBehind="Home.aspx.cs" Inherits="Stagewebsite2._0.Home" %>
 <asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+    &nbsp;&nbsp;Homepage
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
-    homes
+    Home
 </asp:Content>
--- Stagewebsite2.0/Stage.Master.designer.cs
+++ Stagewebsite2.0/Stage.Master.designer.cs
@@ -13,15 +13,6 @@ namespace Stagewebsite2._0 {
     public partial class Stage {
         
         /// <summary>
-        /// head control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.ContentPlaceHolder head;
-        
-        /// <summary>
         /// form1 control.
         /// </summary>
         /// <remarks>
@@ -29,6 +20,15 @@ namespace Stagewebsite2._0 {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+        
+        /// <summary>
+        /// head control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder head;
         
         /// <summary>
         /// ContentPlaceHolder1 control.
